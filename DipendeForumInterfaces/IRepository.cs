f6d9ff73212e2eb71edc0f0efca3ba4311da7a6c
Simone camilletti <Simone.Camilletti@live.it>
2021-04-06 17:59:22 +0200

    Gio

--- DipendeForumInterfaces/IRepository.cs
+++ DipendeForumInterfaces/IRepository.cs
@@ -4,13 +4,13 @@ using System.Collections.Generic;
 
 namespace DipendeForumInterfaces
 {
-    public interface IRepository
+    public interface IRepository<T>
     {
         public void Add ();
         public void Delete();
         public void Update();
-        public IEnumerable GetAll();
-        public  IEnumerable GetById();
+        public IEnumerable<T> GetAll();
+        public  T GetById();
     }
     
 }
