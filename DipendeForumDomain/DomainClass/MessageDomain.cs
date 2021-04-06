6e701723cb16d5c89698e24b4a8b79ffc996cf95
Simone camilletti <simone.camilletti@live.it>
2021-04-06 17:28:11 +0200

    Sistemato Business

--- DipendeForumDomain/DomainClass/MessageDomain.cs
+++ DipendeForumDomain/DomainClass/MessageDomain.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace DipendeForum.Context.Entities
+namespace DipendeForumDomain.DomainClass
 {
     public class MessageDomain
     {
--- DipendeForumDomain/DomainClass/UserDomain.cs
+++ DipendeForumDomain/DomainClass/UserDomain.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using DipendeForum.Context.Entities;
 
-namespace DipendeForumDomain
+namespace DipendeForumDomain.DomainClass
 {
     public class UserDomain
     {
