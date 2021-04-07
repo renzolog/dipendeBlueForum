aaec0aa71118a5f3c33032aefdc89d81dcc8c81f
Simone camilletti <Simone.Camilletti@live.it>
2021-04-07 23:50:18 +0200

    Repository quasi finita

--- DipendeForum.Repositories/Repositories/Repository.cs
+++ DipendeForum.Repositories/Repositories/Repository.cs
@@ -1,30 +1,126 @@
-﻿using DipendeForumInterfaces;
+﻿using DipendeForum.Context.Entities;
+using DipendeForumInterfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using DipendeForum.Context;
 
 namespace DipendeForum.Repositories.Repositories
 {
     public class Repository<T> : IRepository<T>
     {
-        public void Add()
+        private readonly ForumtDbContext _ctx;
+        public Repository(ForumDbContext ctx)
         {
-            throw new NotImplementedException();
+            _ctx = ctx;
+        }
+        public void Add(T obj)
+        {
+            switch (typeof(T).Name)
+            {
+                case ("User"):
+                    _ctx.User.Add(obj);
+                    break;                
+
+                case ("Post"):
+                    _ctx.Post.Add(obj);
+                    break;            
+                
+                case ("Message"):
+                    _ctx.User.Add(obj);
+                    break;
+                
+                Default:
+                    throw new Exception("Mbecille che hai postato?");
+                    break;
+            }
+                    _ctx.SaveChanges();
+            
         }
 
-        public void Delete()
+        public void Delete( int id)
         {
-            throw new NotImplementedException();
+            switch (typeof(T).Name) { 
+            case ("User"):
+            var toDeleteUser = _ctx.User.Where(u=>u.Id == id).FirstOrDefault();
+            _ctx.User.Remove(toDeleteUser);
+            break;                
+
+            case ("Post"):
+                var toDeletePost = _ctx.Post.Where(u => u.Id == id).FirstOrDefault();
+                _ctx.Post.Remove(toDeletePost);
+
+                    break;            
+                
+            case ("Message"):
+                var toDeleteMessage = _ctx.Message.Where(u => u.Id == id).FirstOrDefault();
+                _ctx.Message.Remove(toDeleteMessage);
+
+                    break;
+
+            Default:
+            throw new Exception("Mbecille che hai postato?");
+            break;
+        }
+            _ctx.SaveChanges();
         }
 
         public IEnumerable<T> GetAll()
         {
-            throw new NotImplementedException();
+            switch (typeof(T).Name)
+            {
+                case ("User"):
+                    var listUser = _ctx.User;
+                    return listUser;
+                    break;
+
+                case ("Post"):
+                    var listPost = _ctx.Post;
+                    return listPost;
+
+                    break;
+
+                case ("Message"):
+                    var listMessage = _ctx.Message;
+                    return listMessage;
+
+                    break;
+
+                    Default:
+                    throw new Exception("Mbecille che hai postato?");
+                    break;
+
+            }
+
+            return null;
         }
 
-        public T GetById()
+        public T GetById(int id)
         {
-            throw new NotImplementedException();
+            switch (typeof(T).Name)
+            {
+                case ("User"):
+                    var User = _ctx.User.Where(u => u.Id == id).FirstOrDefault();
+                    return User;
+                    break;
+
+                case ("Post"):
+                    var Post = _ctx.Post.Where(u => u.Id == id).FirstOrDefault();
+                    return Post;
+                    break;
+
+                    break;
+
+                case ("Message"):
+                    var Message = _ctx.Message.Where(u => u.Id == id).FirstOrDefault();
+                    return Message;
+
+                    break;
+
+                   
+
+            }
+
         }
 
         public void Update()
